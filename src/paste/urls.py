689ab004b5fb1a9ec0611ca60f247e141dc19b3d
Abdujabbar MIRKHALIKOV <abdujabbar.mirkhalikov@prestataire.sewan.fr>
2022-08-17 15:46:24 +0500

    Added HelloView for debug

--- src/paste/urls.py
+++ src/paste/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import PasteRetriveAPIView, PasteCreateAPIView, index
+from .views import PasteRetriveAPIView, PasteCreateAPIView, HelloView
 
 urlpatterns = [
-    path('', index),
+    path('', HelloView.as_view()),
     path('store', PasteCreateAPIView.as_view()),
     path('<int:pk>', PasteRetriveAPIView.as_view()),
 ]
--- src/paste/views.py
+++ src/paste/views.py
@@ -1,12 +1,22 @@
 from .services import SharePasteService
 from django.http import HttpResponse
 from rest_framework.generics import RetrieveAPIView, CreateAPIView
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Paste
 from .serializers import PasteSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello World')
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,) 
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
 
 
 
@@ -16,6 +26,8 @@ class PasteRetriveAPIView(RetrieveAPIView):
 
 
 class PasteCreateAPIView(CreateAPIView):
+    permission_classes = (IsAuthenticated,) 
+    authentication_classes = (TokenAuthentication,) 
     queryset = Paste.objects.all()
     serializer_class = PasteSerializer
 
